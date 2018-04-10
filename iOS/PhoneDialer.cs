78ee9016c9607cfe51644d301caad81d01282fba
Jason Weaver <jason.weaver@utexas.edu>
2018-04-10 18:49:34 -0400

    Initial check-in of module Phoneword

---
+++ iOS/PhoneDialer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Phoneword.iOS;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+namespace Phoneword.iOS
+{
+    public class PhoneDialer : IDialer
+    {
+        public bool Dial(string number)
+        {
+            return UIApplication.SharedApplication.OpenUrl(
+                new Foundation.NSUrl("tel:" + number));
+        }
+    }
+}
